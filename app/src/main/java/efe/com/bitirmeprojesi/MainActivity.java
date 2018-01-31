81b33b1157fc26235c071a1560d3f6ede04b0c9f
Mustafa EFE <m.efe_1905@hotmail.com>
2018-01-31 10:49:45 +0300

    Kayıt olan kullanıcıya doğrulama maili gönderme fonksiyonu yazıldı.

--- app/src/main/java/efe/com/bitirmeprojesi/MainActivity.java
+++ app/src/main/java/efe/com/bitirmeprojesi/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,7 +16,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    
 
     //Arayüzdeki degiskenler
     private EditText mEmailDegisken;
@@ -36,10 +36,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //Butonlar
         findViewById(R.id.btn_giris).setOnClickListener(this);
         findViewById(R.id.btn_kayit_ol).setOnClickListener(this);
+        findViewById(R.id.btn_mail_gonder).setOnClickListener(this);
 
         //FirebaseAuth nesne değişkeni örneklendiriliyor.
         mAuth = FirebaseAuth.getInstance();
-
     }
 
     @Override
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     //firebase yardımı ile kullanıcıları kayıt eder.
     private void hesapOlustur(String email, String password){
+        dinleyiciler();
         if (!formuDogrula()){
             return;
         }
@@ -63,6 +64,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             //Kayıt başarılı ise, arayüzü kullanıcı bilgileri ile güncelle.
                             FirebaseUser user = mAuth.getCurrentUser();
                             arayuzuGuncelle(user);
+
                         }else{
                             //Kayıt başarısız ise arayüzü null ile güncelle
                             Toast.makeText(MainActivity.this, "Authentication failed.",
@@ -73,11 +75,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 });
     }
 
+    private void dogrulamaMailiGonder() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user != null){
+            user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        MainActivity.this.setTitle("Mail gönderildi");
+                    }else{
+                        MainActivity.this.setTitle("Mail gönderilemedi");
+                    }
+                }
+            });
+        }
+    }
+
+    private void dinleyiciler(){
+        mAuth.addAuthStateListener( new FirebaseAuth.AuthStateListener(){
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    dogrulamaMailiGonder();
+                }
+            }
+        });
+    }
+
     //email ve parolanın belirtilen kriterlere göre girilip girilmediğini kontrol eder.
     private boolean formuDogrula() {
         boolean valid = true;
 
-        String email = mEmailDegisken.getText().toString();
+        String email = mEmailDegisken.getText().toString().trim();
         if(TextUtils.isEmpty(email)){
             mEmailDegisken.setError("Gerekli");
             valid = false;
@@ -115,9 +145,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         int i = view.getId();
         if(i == R.id.btn_giris){
-            //signIn(mEmailDegisken.getText().toString(), mParolaDegisken.getText().toString());
+            //girisYap(mEmailDegisken.getText().toString(), mParolaDegisken.getText().toString());
         }else if(i == R.id.btn_kayit_ol){
             hesapOlustur(mEmailDegisken.getText().toString(), mParolaDegisken.getText().toString());
+        }else if(i == R.id.btn_mail_gonder){
+            dogrulamaMailiGonder();
         }
     }
 }
